7b2afc42e26f785c4b9ff11810fdf5fbacd5bb5c
Đỗ Thắng <dominhthang@os-MacBook-Pro.local>
2018-11-28 12:28:48 +0700

    Initial Commit

---
+++ Assignment2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Assignment2
+//
+//  Created by Đỗ Minh Thắng on 11/28/18.
+//  Copyright © 2018 Đỗ Minh Thắng. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
